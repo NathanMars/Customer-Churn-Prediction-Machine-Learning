ac8e856bbe5245ddab86b8d8ca98499b78d218d1
NathaMars <nathanmarques.silva@gmail.com>
2025-12-05 23:17:07 -0300

    Chore: identificamos o melhor modelo para o problema

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 # Genero -> 1 = Feminino, 2 = Masculino
 # Churn -> 1 = Sim, 2 = Não
 # O Scaler é exportado como scaler.pkl
+# O modelo ideal para o problema é identificado no Jupiter Notebook e exportado como model.pkl
+# A ordem dos dados em X são Age', 'Gender', 'Tenure', 'MonthlyCharges'
